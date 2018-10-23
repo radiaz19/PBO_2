7b59a56847539279a0779c83e27e73e04439ce4b
Radiaz <43472432+radiaz19@users.noreply.github.com>
2018-10-23 22:34:36 +0700

    Add files via upload

---
+++ src/PBO_2/pbo_2.java
@@ -0,0 +1,17 @@
+package PBO_2;
+
+import java.util.Scanner;
+
+public class pbo_2 {
+
+    public static void main(String[] args) {
+        int i,input;
+        Scanner x = new Scanner(System.in);
+    do{
+        System.out.println("1.Input Data");
+        System.out.println("2.Tampilkan Data");
+        System.out.println("3.Keluar");
+        System.out.print("Masukkan Pilihan ");input = x.nextInt();
+    }while(input!=3);
+}
+}
